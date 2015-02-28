ef0805aebc13d2c4de7fed3a9b36caef06a5bdd5
ejimenezdelgado <ejimenezdelgado@hotmail.com>
2015-02-27 20:28:40 -0600

    Clase 14

---
+++ Clases/Clase 8/MecanicaUTN/MecanicaUTN/Logica/Sql/RepuestosSql.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MecanicaUTN.AccesoDatos;
+using MecanicaUTN.Logica.Intefaces;
+using MecanicaUTN.Logica.SqlServer;
+using MecanicaUTN.Entidades;
+using MecanicaUTN.Logica.Postgres;
+
+
+namespace MecanicaUTN.Logica
+{
+    public class RepuestosSql:IRepuestoSql
+    {
+        /**/
+        public Boolean HayError { set; get; }
+        public String DescripcionError { set; get; }
+
+        public IRepuestoSql ObtenerInstancia()
+        {
+            IRepuestoSql repuestoSql=null;
+            switch (AccesoDatos.AccesoDatos.Instance.accesoDatos.ContextDataBase)
+            {
+                case ContextDataBase.SqlServer:
+                    repuestoSql = new RepuestoSqlServer();
+                    break;
+                case ContextDataBase.PostgreSql:
+                    repuestoSql = new RepuestoPostgres();
+                    break;
+                default:
+                    break;
+            }
+            return repuestoSql;
+        }
+
+        public void AgregarRepuesto(Repuesto repuesto)
+        {
+            IRepuestoSql repuestoSql = this.ObtenerInstancia();
+            repuestoSql.AgregarRepuesto(repuesto);
+            if(repuestoSql.HayError)
+            {
+                this.HayError = true;
+                this.DescripcionError = repuestoSql.DescripcionError;
+            }
+        }
+
+        public void EditarRepuesto(int id, Repuesto repuesto)
+        {
+            IRepuestoSql repuestoSql = this.ObtenerInstancia();
+            repuestoSql.EditarRepuesto(id,repuesto);
+            if (repuestoSql.HayError)
+            {
+                this.HayError = true;
+                this.DescripcionError = repuestoSql.DescripcionError;
+            }
+        }
+
+        public List<Repuesto> TraerRepuestos()
+        {
+            IRepuestoSql repuestoSql = this.ObtenerInstancia();
+            List<Repuesto> repuestos= repuestoSql.TraerRepuestos();
+            if (repuestoSql.HayError)
+            {
+                this.HayError = true;
+                this.DescripcionError = repuestoSql.DescripcionError;
+            }
+            return repuestos;
+        }
+
+
+        public void EliminarRepuesto(int id)
+        {
+            IRepuestoSql repuestoSql = this.ObtenerInstancia();
+            repuestoSql.EliminarRepuesto(id);
+            if (repuestoSql.HayError)
+            {
+                this.HayError = true;
+                this.DescripcionError = repuestoSql.DescripcionError;
+            }
+        }
+    }
+}
---
+++ Clases/Clase 8/MecanicaUTN/MecanicaUTN/Logica/Sql/UsuarioSql.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MecanicaUTN.AccesoDatos;
+using MecanicaUTN.Logica.Intefaces;
+using MecanicaUTN.Logica.SqlServer;
+using MecanicaUTN.Entidades;
+using MecanicaUTN.Logica.Postgres;
+
+
+namespace MecanicaUTN.Logica
+{
+    public class UsuarioSql:IUsuarioSql
+    {
+        /**/
+        public Boolean HayError { set; get; }
+        public String DescripcionError { set; get; }
+
+        public IUsuarioSql ObtenerInstancia()
+        {
+            IUsuarioSql usuarioSql = null;
+            switch (AccesoDatos.AccesoDatos.Instance.accesoDatos.ContextDataBase)
+            {
+                case ContextDataBase.SqlServer:
+                    usuarioSql = null;
+                    break;
+                case ContextDataBase.PostgreSql:
+                    usuarioSql = new UsuarioPostgres();
+                    break;
+                default:
+                    break;
+            }
+            return usuarioSql;
+        }
+
+        public void AgregarUsuario(Usuario usuario)
+        {
+            IUsuarioSql usuarioSql = this.ObtenerInstancia();
+            usuarioSql.AgregarUsuario(usuario);
+            if (usuarioSql.HayError)
+            {
+                this.HayError = true;
+                this.DescripcionError = usuarioSql.DescripcionError;
+            }
+        }
+
+        public Usuario ValidarUsuario(Usuario usuario)
+        {
+            IUsuarioSql usuarioSql = this.ObtenerInstancia();
+            Usuario usuarioSelecionado= usuarioSql.ValidarUsuario(usuario);
+            if (usuarioSql.HayError)
+            {
+                this.HayError = true;
+                this.DescripcionError = usuarioSql.DescripcionError;
+            }
+            return usuarioSelecionado;
+        }
+    }
+}
